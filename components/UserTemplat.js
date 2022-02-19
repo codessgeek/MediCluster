6a706498a68e17214bb3995862c63def26968501
JPonda <pondajay637@gmail.com>
2022-02-19 22:45:48 +0530

    create simple interface

--- components/UserTemplat.js
+++ components/UserTemplat.js
@@ -1,12 +1,83 @@
 import React from 'react'
-import { View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-const UserTemplat = () => {
+const UserTemplat = ({ name, gender, status, edit }) => {
+  let color = "green"
+  switch (status[0]) {
+    case status[1]:
+      color = 'red'
+      break;
+    case status[2]:
+      color = 'yellow'
+      break;
+    default:
+      break;
+  }
+  let limit = 18
+
   return (
-    <View>
-        
-    </View>
+    <>
+      <View style={styles.container}>
+        <View>
+          <View style={styles.img}>
+            <Icon name="account-plus" size={40} color="rgb(255, 255, 255)" />
+          </View>
+          <View style={{ display: `${color === 'green' ? 'none' : 'flex'}`, ...styles.dot, backgroundColor: `${color}` }}>
+          </View>
+        </View>
+
+        <View style={styles.details}>
+          <View style={styles.line}>
+            <Text style={styles.text}>{`${name.length > limit ? name.slice(0, limit - 3) + "..." : name}`}</Text>
+          </View>
+        </View>
+      </View>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: 7,
+    padding: 5,
+    marginVertical: 2,
+    marginHorizontal: 5,
+    backgroundColor: 'blue'
+  },
+  img: {
+    backgroundColor: 'pink',
+    borderRadius: 25,
+    alignSelf: 'flex-start',
+    padding: 5,
+    marginRight: 5
+  },
+  details: {
+    //backgroundColor: 'red',
+    padding: 5,
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '700',
+    marginRight: 15,
+    textTransform: 'capitalize'
+  },
+  line: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+  },
+  dot: {
+    position: 'absolute',
+    bottom: 0,
+    right: 5,
+    height: 15,
+    width: 15,
+    borderRadius: 20,
+    backgroundColor: 'red'
+  }
+})
 
 export default UserTemplat;
--- screens/DisplayAllUser.js
+++ screens/DisplayAllUser.js
@@ -1,12 +1,56 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, FlatList, Text, ScrollView } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import UserTemplat from '../components/UserTemplat'
 
 const DisplayAllUser = () => {
+  const status = [0, 1]
+  const data = [
+    { id: 0, name: "charli", gender: 'male' },
+    { id: 1, name: "suzi", gender: 'femail' },
+    { id: 2, name: "braw", gender: 'not mentaion' },
+    { id: 3, name: "braw", gender: 'not mentaion' },
+  ]
+  let edit = false
   return (
-    <View>
-
+    <View style={Styles.container}>
+      <View style={Styles.createuser}>
+        <Icon name="account-plus" size={35} color="rgb(255, 255, 255)" style={Styles.icn} />
+      </View>
+      <View style={Styles.userlist}>
+        <FlatList
+          data={data}
+          renderItem={({ item, index }) => <UserTemplat name={item.name} gender={item.gender} status={[index, ...status]} edit={edit} />}>
+        </FlatList>
+      </View>
     </View>
   )
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgb(225, 225, 225)',
+    padding: 5,
+  },
+  createuser: {
+    padding: 15,
+    alignItems: 'flex-end'
+
+  },
+  icn: {
+    padding: 5,
+
+    alignSelf: 'auto',
+
+    borderRadius: 30,
+
+    backgroundColor: 'rgb(100, 100, 100)',
+  },
+  userlist: {
+
+  }
+})
 
 export default DisplayAllUser
