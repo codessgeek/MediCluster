81fe795c80db6a7e85d17bafb7e6462474df4d73
JPonda <pondajay637@gmail.com>
2022-03-31 00:23:22 +0530

    add color folder

---
+++ assets/color/Colors.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const color = {
+
+    // background colors
+    'primary_bg': '#fff',
+    'secondary_bg': "rgba(200, 200, 200, .5)",
+    'bg_1': 'rgba(183,183,183,1)',
+
+    // text colors
+    'primary_text': 'black',
+    'secondary_text': 'rgba(114,113,113,1)',
+
+    // icon colors
+    'primary_icon': 'black',
+    'secondary_icon': 'rgba(59,59,59,1)',
+
+    // icon background
+    'primary_iconbg': 'rgba(0, 0, 0, .2)',
+    'secondary_iconbg': '',
+
+    // border colors
+    'primary_border': 'black',
+    'secondary_border': 'gray'
+}
+
+export default color
