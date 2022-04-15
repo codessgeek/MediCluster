e47f2d18859d7b2f4d5c7d33e86ba42eee8295e1
JPonda <pondajay637@gmail.com>
2022-04-15 08:02:15 +0530

    --amend

--- components/ScheduleScreenComp/FilterCom.js
+++ components/ScheduleScreenComp/FilterCom.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react';
-import {Text, View, TextInput, StyleSheet} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { Text, View, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 const FilterCom = () => {
   const [pickerValue, setpickerValue] = useState(1);
   return (
-    <View style={styles.filteroptionpanel}>
+    <SafeAreaView style={styles.filteroptionpanel}>
       <View style={styles.fobg}>
         <View style={styles.filterRow}>
           <Text style={styles.filter}>Filter:</Text>
           <Picker
             style={styles.dropdown}
+            itemStyle={styles.dropdownitem}
             selectedValue={pickerValue}
             onValueChange={item => setpickerValue(item)}>
+            <Picker.Item label="item 1" value="1" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 1" value="1" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 1" value="1" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 1" value="1" />
+            <Picker.Item label="item 2" value="2" />
+            <Picker.Item label="item 2" value="2" />
             <Picker.Item label="item 1" value="1" />
             <Picker.Item label="item 2" value="2" />
             <Picker.Item label="item 2" value="2" />
@@ -23,10 +36,10 @@ const FilterCom = () => {
           <Text style={styles.filter}>Select item:</Text>
           <TextInput
             placeholder="choose here"
-            style={styles.chooseHere}></TextInput>
+            style={styles.chooseHere} />
         </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -57,14 +70,17 @@ const styles = StyleSheet.create({
   },
   dropdown: {
     fontFamily: 'roboto-regular',
-    color: '#121212',
-    height: 10,
+    height: 20,
     flexGrow: 1,
     marginLeft: 5,
-    borderColor: 'green',
-    borderWidth: 2,
-    backgroundColor: 'yellow',
+    color: 'black',
+    backgroundColor: 'pink'
   },
+  dropdownitem: {
+    height: 20,
+    color: 'green',
+    fontSize: 25
+  }
 });
 
 export default FilterCom;
